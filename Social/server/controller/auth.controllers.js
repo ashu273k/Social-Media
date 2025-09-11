20d86eefca546349c6e91552b2b400c22007117e
ashu273k <ashu9472.as@gmail.com>
2025-09-11 18:59:21 +0530

    Sing in & SIng Up

--- Social/server/controller/auth.controllers.js
+++ Social/server/controller/auth.controllers.js
@@ -1,7 +1,8 @@
 import User from "../model/user.model.js";
+import bcrypt from "bcryptjs";
 
 export const signUp = async (req, res) => {
-  const { name, userName, email, password } = req.body;
+  const { name, email,  password, userName } = req.body;
 
   if(!name || !email || !userName || !password) {
     return res.status(400).json({ message: "All fields are required" });
@@ -17,6 +18,36 @@ export const signUp = async (req, res) => {
     return res.status(400).json({ message: "Username already taken" });
   }
 
-  const newUser = await User.create({ name, userName, email, password });
-  res.status(201).json({ message: "User created successfully" });
+  if (password.length < 6) {
+    return res.status(400).json({ message: "Password must be at least 6 characters long" });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  console.log(salt);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newUser = await User.create({ name, email, password: hashedPassword, userName });
+  res.status(201).json(newUser);
+}
+
+export const signIn = async (req, res) => {
+  const { userName, password} = req.body;
+  
+
+  if(!userName || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const user = await User.findOne({userName})
+  console.log(user);
+  if(!user) {
+    return res.status(400).json({ message: "Invalid UserName" });
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(password, user.password)
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ message: "Invalid Password" });
+  }
+
+  res.status(200).json({ message: "Sign in successful" });
 }
--- Social/server/index.js
+++ Social/server/index.js
@@ -1,12 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import authRouter from './routes/auth.routes.js'
 
 dotenv.config()
 
 const app = express()
 const PORT = 8000
 app.use(express.json())
+
+
+// Authentication routes
+app.use('/api/auth', authRouter);
 
 connectDB()
 app.get('/', (req, res) => {
