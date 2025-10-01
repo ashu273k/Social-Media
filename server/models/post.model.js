6e62a7ae12c9e915b33a947d0231abc32c900308
Aashu Kumar <ashu9472.as@gmail.com>
2025-10-01 12:15:59 +0530

    Identation change

--- server/models/post.model.js
+++ server/models/post.model.js
@@ -1,61 +1,64 @@
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 
-const postSchema = new mongoose.Schema({
-  // author
-  // caption
-  // content - image , video
-  // Date and time
-  // Likes
-  // Comments
+const postSchema = new mongoose.Schema(
+  {
+    // author
+    // caption
+    // content - image , video
+    // Date and time
+    // Likes
+    // Comments
 
-  author: {
-    type: Mongoose.Schema.Types.ObjectId,
-    ref: "user",
-    required: true,
-  },
-
-  caption: {
-    type: String,
-    default: "",
-  },
-
-  mediaType: {
-    type: String,
-    enum: ["image", "video"],
-    required: true,
-  },
-
-  mediaUrl: {
-    type: String,
-    required: true,
-  },
-
-  likes: [
-    {
-      type: Mongoose.Schema.Types.ObjectId,
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
       ref: "user",
+      required: true,
     },
-  ],
 
-  comments: [
-    {
-      user: {
-        type: Mongoose.Schema.Types.ObjectId,
+    caption: {
+      type: String,
+      default: "",
+    },
+
+    mediaType: {
+      type: String,
+      enum: ["image", "video"],
+      required: true,
+    },
+
+    mediaUrl: {
+      type: String,
+      required: true,
+    },
+
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
         ref: "user",
       },
+    ],
 
-      text:{
-        type:String,
-        required:true
+    comments: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "user",
+        },
+
+        text: {
+          type: String,
+          required: true,
+        },
+
+        createdAt: {
+          type: Date,
+          default: Date.now(),
+        },
       },
-
-      createdAt:{
-        type:Date,
-        default:Date.now()
-      }
-    },
-  ],
-} , {timestamps:true});
+    ],
+  },
+  { timestamps: true }
+);
 
 const Post = mongoose.model("post", postSchema);
 
