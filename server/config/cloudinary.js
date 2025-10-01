a2fa95abd7f122235488f0816ddd9e8f3e7acaf4
Aashu Kumar <ashu9472.as@gmail.com>
2025-10-01 15:17:07 +0530

    Cloudinary setup file

---
+++ server/config/cloudinary.js
@@ -0,0 +1,19 @@
+import { v2 as cloudinary } from "cloudinary";
+
+const uploadFile = async (file) => {
+  try {
+    cloudinary.config({
+      cloud_name: process.env.CLOUD_NAME,
+      api_key: process.env.CLOUD_API_KEY,
+      api_secret: process.env.CLOUD_API_SECRET
+    });
+
+    const result = await cloudinary.uploader.upload(file)
+    return result.secure_url
+
+  } catch (error) {
+    console.error("Error uploading file to Cloudinary:", error);
+  }
+};
+
+export default uploadFile
